2e9977fef05f7b8761972becf90b87c7ec29c36d
Pete <11158905+Pittawat2542@users.noreply.github.com>
2021-10-08 19:30:47 +0700

    feat: setup github page deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== 'production'
+
 module.exports = {
+  assetPrefix: !debug ? '/superposition/' : '',
   reactStrictMode: true,
 }
