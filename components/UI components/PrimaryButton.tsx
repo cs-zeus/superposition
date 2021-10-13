ec0675643d75656475a2ac2e862ff84f42a59d9d
zielu92 <zielu0@gmail.com>
2021-10-13 15:04:00 +0700

    feat: UI component primarybutton

---
+++ components/UI components/PrimaryButton.tsx
@@ -0,0 +1,14 @@
+import styled, {css} from 'styled-components';
+
+const PrimaryButton = styled.button`
+  font-size: 1em;
+  font-weight: bold;
+  margin: 1em;
+  padding: 0.4em 2em;
+  border-radius: 18px;
+  background: #FC85AE;
+  color: #9E579D;
+  border: 0;
+`;
+
+export default PrimaryButton;
