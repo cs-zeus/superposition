71339ac1aad5f334a104341f0f92c335c4dde0f4
Pete <11158905+Pittawat2542@users.noreply.github.com>
2021-10-13 21:00:39 +0700

    reactor: extract app config and charges hooks

---
+++ hooks/usePointCharges.ts
@@ -0,0 +1,34 @@
+import { PointCharge } from "cs-zeus";
+import { useState } from "react";
+
+export const usePointCharges = () => {
+  const initialCharges = [
+    { name: 'Test Charge', position: { x: 0, y: 0 }, q: 1 },
+  ];
+
+  const [pointCharges, setPointCharges] = useState<PointCharge[]>(initialCharges);
+
+  const addPointChargeHandler = (q: number) => {
+    setPointCharges((prev) => [...prev, { name: `Charge ${prev.length + 1}`, position: { x: 0, y: 0 }, q }]);
+  };
+
+  const editChargeHandler = (charge: PointCharge) => {
+    setPointCharges((prev) => [
+      ...prev.filter((c) => c.name !== charge.name),
+      charge,
+    ]);
+  };
+
+  const editPositionHandler = (charge: PointCharge) => {
+    setPointCharges((prev) => [
+      ...prev.filter((c) => c.name !== charge.name),
+      charge,
+    ]);
+  };
+
+  const removePointChargeHandler = (chargeName: string) => {
+    setPointCharges((prev) => prev.filter((charge) => charge.name !== chargeName));
+  };
+
+  return [pointCharges, addPointChargeHandler, editChargeHandler, editPositionHandler, removePointChargeHandler];
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,61 +1,31 @@
+import { PointCharge, TestCharge } from 'cs-zeus';
+
 import { AppConfig } from '../types/AppConfig';
 import Head from 'next/head';
 import type { NextPage } from 'next';
-import { PointCharge } from 'cs-zeus';
 import SimulationPane from '../components/SimulationPane/SimulationPane';
-import { useState } from 'react';
+import styled from 'styled-components';
+import { useAppConfig } from '../hooks/useAppConfig';
+import { usePointCharges } from '../hooks/usePointCharges';
 
 const Home: NextPage = () => {
-	const initialAppConfig: AppConfig = {
-		hasGridLineEnabled: true,
-		isShowModal: false,
-	};
+	const [
+		appConfig,
+		toggleGridLineHandler,
+		openModalHandler,
+		closeModalHandler,
+	] = useAppConfig();
 
-	const [charges, setCharges] = useState<PointCharge[]>([
-		{ name: 'Test Charge', position: { x: 1, y: 1 }, q: -1 },
-		{ name: 'Charge 1', position: { x: 0, y: 0 }, q: 1 },
-	]);
-	const [appConfig, setAppConfig] = useState<AppConfig>(initialAppConfig);
-
-	const addPointChargeHandler = (charge: PointCharge) => {
-		setCharges((prev) => [...prev, charge]);
-	};
-
-	const editChargeHandler = (charge: PointCharge) => {
-		setCharges((prev) => [
-			...prev.filter((c) => c.name !== charge.name),
-			charge,
-		]);
-	};
-
-	const editPositionHandler = (charge: PointCharge) => {
-		setCharges((prev) => [
-			...prev.filter((c) => c.name !== charge.name),
-			charge,
-		]);
-	};
-
-	const removePointChargeHandler = (chargeName: string) => {
-		setCharges((prev) => prev.filter((charge) => charge.name !== chargeName));
-	};
-
-	const toggleGridLineHandler = () => {
-		setAppConfig((prev) => ({
-			...prev,
-			hasGridLineEnabled: !prev.hasGridLineEnabled,
-		}));
-	};
-
-	const openModalHandler = () => {
-		setAppConfig((prev) => ({ ...prev, isShowModal: true }));
-	};
-
-	const closeModalHandler = () => {
-		setAppConfig((prev) => ({ ...prev, isShowModal: false }));
-	};
+	const [
+		pointCharges,
+		addPointChargeHandler,
+		editChargeHandler,
+		editPositionHandler,
+		removePointChargeHandler,
+	] = usePointCharges();
 
 	return (
-		<div>
+		<>
 			<Head>
 				<title>Superposition</title>
 				<meta
@@ -64,16 +34,42 @@ const Home: NextPage = () => {
 				/>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-
-			<SimulationPane
-				appConfig={appConfig}
-				charges={charges.filter((charge) => charge.name !== 'Test Charge')}
-				testCharge={
-					charges.filter((charge) => charge.name === 'Test Charge')[0]
-				}
-			/>
-		</div>
+			<Wrapper>
+				<LeftPane>
+					<SimulationPane
+						appConfig={appConfig as AppConfig}
+						charges={(pointCharges as PointCharge[]).filter(
+							(charge) => charge.name !== 'Test Charge'
+						)}
+						testCharge={
+							(pointCharges as PointCharge[]).filter(
+								(charge) => charge.name === 'Test Charge'
+							)[0] as TestCharge
+						}
+					/>
+				</LeftPane>
+				<RightPane>
+					ConfigurationPane
+				</RightPane>
+			</Wrapper>
+		</>
 	);
 };
+
+const Wrapper = styled.div`
+	display: flex;
+	height: 100vh;
+	position: fixed;
+	width: 100%;
+`;
+
+const LeftPane = styled.div`
+	flex: 75;
+`;
+
+const RightPane = styled.div`
+	flex: 25;
+	overflow: auto;
+`;
 
 export default Home;
