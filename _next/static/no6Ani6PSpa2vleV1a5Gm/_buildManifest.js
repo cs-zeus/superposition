31ac3f34728ba09e3dc0841f50f9849cbdc899c1
keiseithunder <keiseithunder@users.noreply.github.com>
2021-10-15 05:07:12 +0000

    deploy: 0e5c8975d45e1313542e2da23f48c25c1fca28ec

--- _next/static/no6Ani6PSpa2vleV1a5Gm/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/592-9b4f0f36c9208ee57f04.js","static/chunks/pages/index-d2de494cf71613d67048.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
