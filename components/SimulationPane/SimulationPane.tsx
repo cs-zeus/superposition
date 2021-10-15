2097e67c9796ff60cdaaa451264182bfb5e4dab7
Pittawat <pittawat2542@gmail.com>
2021-10-15 12:04:10 +0700

    fix: change styled-component to emotion to fix HoC not apply style

--- components/SimulationPane/SimulationPane.tsx
+++ components/SimulationPane/SimulationPane.tsx
@@ -1,7 +1,7 @@
 import { PointCharge, TestCharge } from 'cs-zeus';
 
 import { AppConfig } from '../../types/AppConfig';
-import styled from 'styled-components';
+import styled from '@emotion/styled';
 import { useInteractive } from '../../hooks/useInteractive';
 
 type SimulationPaneProps = {
@@ -209,50 +209,50 @@ const Wrapper = styled(InteractiveWrapper)`
 	width: 100%;
 
 	.interactive .line {
- 		stroke: #fbfafa;
- 	}
- 	.interactive .circle {
- 		fill: none;
- 		stroke: #fbfafa;
- 		stroke-width: 2px;
- 	}
- 	.interactive .circle.point {
- 		stroke: none;
- 	}
- 	.interactive .circle.positive {
- 		stroke: none;
- 		fill: #eb5757;
- 	}
- 	.interactive .circle.negative {
- 		stroke: none;
- 		fill: #56ccf2;
- 	}
- 	.interactive .circle.test-charge {
- 		stroke: #fbfafa;
- 		stroke-dasharray: 6;
- 		stroke-width: 3px;
- 	}
- 	.interactive .text {
- 		fill: #fbfafa;
- 		stroke: none;
- 	}
- 	.interactive .text.positive,
- 	.interactive .text.negative {
- 		fill: #303a52;
- 		font-size: 2.5rem;
- 	}
- 	.interactive .rectangle {
- 		stroke: #fbfafa;
- 		stroke-width: 2px;
- 		fill: none;
- 	}
- 	.interactive .path {
- 		fill: #fbfafa;
- 	}
- 	.interactive .arrow {
- 		fill: #fbfafa;
- 		stroke: none;
- 	}
+		stroke: #fbfafa;
+	}
+	.interactive .circle {
+		fill: none;
+		stroke: #fbfafa;
+		stroke-width: 2px;
+	}
+	.interactive .circle.point {
+		stroke: none;
+	}
+	.interactive .circle.positive {
+		stroke: none;
+		fill: #eb5757;
+	}
+	.interactive .circle.negative {
+		stroke: none;
+		fill: #56ccf2;
+	}
+	.interactive .circle.test-charge {
+		stroke: #fbfafa;
+		stroke-dasharray: 6;
+		stroke-width: 3px;
+	}
+	.interactive .text {
+		fill: #fbfafa;
+		stroke: none;
+	}
+	.interactive .text.positive,
+	.interactive .text.negative {
+		fill: #303a52;
+		font-size: 2.5rem;
+	}
+	.interactive .rectangle {
+		stroke: #fbfafa;
+		stroke-width: 2px;
+		fill: none;
+	}
+	.interactive .path {
+		fill: #fbfafa;
+	}
+	.interactive .arrow {
+		fill: #fbfafa;
+		stroke: none;
+	}
 `;
 
 export default SimulationPane;
