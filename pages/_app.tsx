7afa18a276c55c23c42689c19c652afbc0c43799
Pittawat <pittawat2542@gmail.com>
2021-10-15 14:58:58 +0700

    fix: place css in wrong place

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import { AppProps } from 'next/app';
-import GlobalStyles from '../components/GlobalStyles';
+import GlobalStyles from '../components/styles/ResetStyles';
+import InteractiveCustomStyles from '../components/styles/InteractiveCustomStyles';
+import InteractiveDefaultStyles from '../components/styles/InteractiveDefaultStyles';
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
 	return (
 		<>
 			<Component {...pageProps} />
-      <GlobalStyles />
+			<GlobalStyles />
+			<InteractiveDefaultStyles />
+			<InteractiveCustomStyles />
 		</>
 	);
 };
