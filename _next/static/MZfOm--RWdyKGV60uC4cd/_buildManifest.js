8da148500b82cc96d195ae69d7894d1927c0e9d1
Pittawat2542 <Pittawat2542@users.noreply.github.com>
2021-10-15 08:34:24 +0000

    deploy: f615a5c10294a1325e6322593f27f9aa131e0645

---
+++ _next/static/MZfOm--RWdyKGV60uC4cd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/466-2071effc83c6e6742740.js","static/chunks/pages/index-feca94b74294477ae02a.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
