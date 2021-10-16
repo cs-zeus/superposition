27b33525a56cf2e490c8b065e037d559fe2e09fd
Michał Zieliński <zielu0@gmail.com>
2021-10-16 17:33:46 +0700

    feat(ui): PrimaryButton (#6)
    * feat: UI component primarybutton
    
    * added IDE files to gitignore
    
    * feat: simulation pane, app config and charges state (#1)
    
    * feat: Setup SimulationPane
    
    * fix: position translation bug
    
    * feat: charges and app config state
    
    * reactor: extract app config and charges hooks
    
    * chore: add babel plugin for styled-components
    
    * feat: make graph size dynamically calculated
    
    * fix: type issues with improper export
    
    * feat: update charges state when position changed
    
    * style: fix styling rules override
    
    * fix: change styled-component to emotion to fix HoC not apply style
    
    * fix: HoC css bug on production
    
    * fix: place css in wrong place
    
    * fix: css selectors issue
    
    * style: add 'Roboto' font
    
    * feat: add arrow line label
    
    * feat(ui): TextLink (#8)
    
    * feat: add TextLink component
    
    * fix: TextLink styling changes
    
    * style: fix TextLink style and optional icon
    
    * style: fix missing style
    
    * fix: update dependency to match current one
    
    * feat rebase, moved component to ui dir, added .idea to gitignore
    
    * feat fix dependency
    
    * feat resolve conflicts
    
    * feat removed .idea folder
    
    * feat: fix styles
    
    Co-authored-by: Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
    Co-authored-by: Pittawat <pittawat2542@gmail.com>
    Co-authored-by: Clay Fox <42699335+LayLinn@users.noreply.github.com>

---
+++ components/ui/PrimaryButton.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+const PrimaryButton = styled.button`
+  font-weight: bold;
+  margin: 15px;
+  padding: 5px 40px;
+  border-radius: 18px;
+  background: #FC85AE;
+  color: #9E579D;
+  border: 0;
+`;
+
+export default PrimaryButton;
