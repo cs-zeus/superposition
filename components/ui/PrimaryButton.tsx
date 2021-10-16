139ee1844a094b787a3c25c522aa018f81a5292b
Pittawat <pittawat2542@gmail.com>
2021-10-16 17:42:25 +0700

    style: refactor to css variables

--- components/ui/PrimaryButton.tsx
+++ components/ui/PrimaryButton.tsx
@@ -1,13 +1,13 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
 
 const PrimaryButton = styled.button`
-  font-weight: bold;
-  margin: 15px;
-  padding: 5px 40px;
-  border-radius: 18px;
-  background: #FC85AE;
-  color: #9E579D;
-  border: 0;
+	font-weight: bold;
+	margin: 16px;
+	padding: 4px 36px;
+	border-radius: 16px;
+	background: var(--accent-color);
+	color: var(--secondary-color);
+	border: 0;
 `;
 
 export default PrimaryButton;
--- components/ui/TextLink.tsx
+++ components/ui/TextLink.tsx
@@ -1,45 +1,44 @@
-import styled from "@emotion/styled";
-import { HelpCircle } from "react-feather";
+import { HelpCircle } from 'react-feather';
+import styled from '@emotion/styled';
 
 type TextLinkProps = {
-  onClickLink: () => void;
-  hasIcon: boolean;
+	onClickLink: () => void;
+	hasIcon: boolean;
 };
 
 const TextLink: React.FC<TextLinkProps> = ({
-  onClickLink,
-  hasIcon,
-  children,
+	onClickLink,
+	hasIcon,
+	children,
 }) => {
-  const StyledSpan = styled.span`
-    ${hasIcon && "margin-left: 0.5rem;"}
-    text-decoration: underline;
-  `;
+	const StyledSpan = styled.span`
+		${hasIcon && 'margin-left: 8px;'}
+		text-decoration: underline;
+	`;
 
-  const StyledP = styled.p`
-    display: flex;
-    align-items: center;
-    font-family: Roboto;
-    font-size: 1.5rem;
-    color: #fbfafa;
+	const StyledP = styled.p`
+		display: flex;
+		align-items: center;
+		font-size: 1.5rem;
+		color: var(--white);
 
-    &:hover {
-      cursor: pointer;
-    }
+		&:hover {
+			cursor: pointer;
+		}
 
-    &:hover ${StyledSpan} {
-      text-decoration: none;
-    }
-  `;
+		&:hover ${StyledSpan} {
+			text-decoration: none;
+		}
+	`;
 
-  const icon = hasIcon ? <HelpCircle size={28} /> : null;
+	const icon = hasIcon ? <HelpCircle size={28} /> : null;
 
-  return (
-    <StyledP onClick={onClickLink}>
-      {icon}
-      <StyledSpan>{children}</StyledSpan>
-    </StyledP>
-  );
+	return (
+		<StyledP onClick={onClickLink}>
+			{icon}
+			<StyledSpan>{children}</StyledSpan>
+		</StyledP>
+	);
 };
 
 export default TextLink;
