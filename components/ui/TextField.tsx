402950ef06056c2eebf7a5e9ca4c7ae32bebcb5c
Michał Zieliński <zielu0@gmail.com>
2021-10-16 22:45:59 +0700

    feat(ui): TextField (#11)
    * feat: resolved conflits
    
    * feat: removed .idea
    
    * feat: changed requested changes used border radius as in figma, provided front-size 1em, used color from GlobalStyles; fixed .gitignore
    
    * feat: added .idea/vsc.xml to .gitignore and removed .idea/vsc.xml

---
+++ components/ui/TextField.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+
+const TextField = styled.input`
+  width: 30px;
+  border-radius: 16px;
+  font-size: 1rem;
+  color: var(--background);
+  border: 0;
+  text-align: center;
+`;
+
+export default TextField;
