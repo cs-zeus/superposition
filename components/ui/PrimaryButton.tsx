cbc800c3d23a2fb9ab4ab5b535a31fcad87f4514
zielu92 <zielu0@gmail.com>
2021-10-16 17:18:36 +0700

    feat: fix styles

--- components/ui/PrimaryButton.tsx
+++ components/ui/PrimaryButton.tsx
@@ -1,10 +1,9 @@
 import styled from "@emotion/styled";
 
 const PrimaryButton = styled.button`
-  font-size: 1em;
   font-weight: bold;
-  margin: 1em;
-  padding: 0.4em 2em;
+  margin: 15px;
+  padding: 5px 40px;
   border-radius: 18px;
   background: #FC85AE;
   color: #9E579D;
