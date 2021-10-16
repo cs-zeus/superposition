e0508afcd6dbaabe64a39e1b8eed5ca31943bd57
laylinn <clayfoxrss@gmail.com>
2021-10-16 14:16:28 +0700

    style: fix TextLink style and optional icon

--- components/ui/TextLink.tsx
+++ components/ui/TextLink.tsx
@@ -1,22 +1,45 @@
 import styled from "styled-components";
+import { HelpCircle } from "react-feather";
 
 type TextLinkProps = {
   onClickLink: () => void;
+  hasIcon: boolean;
 };
 
-const TextLink: React.FC<TextLinkProps> = ({ onClickLink, children }) => {
+const TextLink: React.FC<TextLinkProps> = ({
+  onClickLink,
+  hasIcon,
+  children,
+}) => {
+  const StyledSpan = styled.span`
+    ${hasIcon && "margin-left: 0.5rem;"}
+    // text-decoration: underline;
+  `;
+
   const StyledP = styled.p`
+    display: flex;
+    align-items: center;
     font-family: Roboto;
+    font-size: 1.5rem;
     color: #fbfafa;
-    text-decoration: underline;
 
     &:hover {
       cursor: pointer;
-      text-decoration:none;
+    }
+
+    &:hover ${StyledSpan} {
+      text-decoration: none;
     }
   `;
 
-  return <StyledP onClick={onClickLink}>{children}</StyledP>;
+  const icon = hasIcon ? <HelpCircle size={28} /> : null;
+
+  return (
+    <StyledP onClick={onClickLink}>
+      {icon}
+      <StyledSpan>{children}</StyledSpan>
+    </StyledP>
+  );
 };
 
 export default TextLink;
