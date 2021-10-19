c7f76b9faa75f1fc22ff530d64d30b04b6f7d594
Wicharn Rueangkhajorn <wicharn.keisei@gmail.com>
2021-10-19 20:50:51 +0700

    feat: total E and total F calculation

--- components/ConfigurationPane/CalculationResultSection.tsx
+++ components/ConfigurationPane/CalculationResultSection.tsx
@@ -1,4 +1,11 @@
-import { PointCharge, TestCharge, getElectricField } from 'cs-zeus'
+import {
+    PointCharge,
+    TestCharge,
+    getElectricField,
+    getElectricForce,
+    getElectricFieldMultiplyByN,
+    getElectricForceMultiplyByN,
+} from 'cs-zeus'
 import {
     getVectorDistance,
     getVectorSize,
@@ -21,55 +28,90 @@ const CalculationResultSection: React.FC<CalculationResultSectionProps> = ({
     charges,
     testCharge,
 }) => {
-    const eTotal = getElectricField(charges, testCharge)
+    const eTotal = getElectricFieldMultiplyByN(charges, testCharge)
+    const fTotal = getElectricForceMultiplyByN(charges, testCharge)
     const eTotalUnitVectorLatex = buildLatexUnitVectorMatrix(
         getUnitVector(eTotal)
     )
+    const fTotalUnitVectorLatex = buildLatexUnitVectorMatrix(
+        getUnitVector(fTotal)
+    )
+    const eTotalWithVectorLatex = `${buildLatexScientificNotation(
+        getVectorSize(eTotal),
+        0,
+        ''
+    )}${eTotalUnitVectorLatex}`
+    const fTotalWithVectorLatex = `${buildLatexScientificNotation(
+        getVectorSize(fTotal),
+        0,
+        ''
+    )}${fTotalUnitVectorLatex}`
     const eComponentEquation = charges.reduce((prevRawLatex, charge) => {
         return (
             prevRawLatex + `\\vec{E}_{${charge.name.replace('Charge', '')}t} + `
         )
     }, '')
-    const eComponentCalculation = charges.reduce((prevRawLatex, charge) => {
-        const r = subtractVector(testCharge.position, charge.position)
-        const unitVectorR = getUnitVector(r)
-        const unitVectorMatrix = buildLatexUnitVectorMatrix(unitVectorR)
-        return (
-            prevRawLatex +
-            `${getVectorSize(getElectricField([charge], testCharge)).toFixed(
-                2
-            )}${unitVectorMatrix} + `
-        )
-    }, '')
+    const eComponentCalculation = charges.reduce(
+        (prevRawLatex, charge, currentIndex, arr) => {
+            const e = getElectricFieldMultiplyByN([charge], testCharge)
+            const eUnitVector = getUnitVector(e)
+            const eUnitVectorLatexMatrix =
+                buildLatexUnitVectorMatrix(eUnitVector)
+            return (
+                prevRawLatex +
+                `${buildLatexScientificNotation(
+                    getVectorSize(
+                        getElectricFieldMultiplyByN([charge], testCharge)
+                    ),
+                    0,
+                    ''
+                )}${eUnitVectorLatexMatrix} ${
+                    (currentIndex + 1) % 2 == 0
+                        ? `$$ \n $$ ${
+                              currentIndex + 1 != arr.length
+                                  ? `\\hspace{1.1cm}`
+                                  : ``
+                          }`
+                        : ``
+                } + `
+            )
+        },
+        ''
+    )
     const eTotalLatex = `$$\\vec{E}_{total} = ${eComponentEquation.substr(
         0,
         eComponentEquation.length - 2
-    )} = ${eComponentCalculation.substr(
+    )}$$\n $$\\hspace{0.88cm} = ${eComponentCalculation.substr(
         0,
         eComponentCalculation.length - 2
-    )} = ${getVectorSize(eTotal)}${eTotalUnitVectorLatex}$$`
-    const fTotalLatex = `$$\\vec{F}_{total} = $$`
+    )}$$\n  $$\\hspace{0.88cm}= ${eTotalWithVectorLatex}\\ \\frac{N}{C}$$`
+    const fEquation = `Q_{t} \\times \\vec{E}_{total}`
+    const fTotalLatex = `$$\\vec{F}_{total} = ${fEquation} = (${testCharge.q} \\times e) \\times ${eTotalWithVectorLatex}$$ \n $$\\hspace{0.86cm} = ${fTotalWithVectorLatex}\\ N$$`
     return (
         <Wrapper>
             <SectionTitle>Calculation</SectionTitle>
             {charges.map(charge => {
-                const r = subtractVector(testCharge.position, charge.position)
+                const r = subtractVector(charge.position, testCharge.position)
                 const rUnitVector = getUnitVector(r)
                 const rUnitVectorLatexMatrix =
                     buildLatexUnitVectorMatrix(rUnitVector)
-                const e = getElectricField([charge], testCharge)
+                const e = getElectricFieldMultiplyByN([charge], testCharge)
                 const eUnitVector = getUnitVector(e)
                 const eUnitVectorLatexMatrix =
                     buildLatexUnitVectorMatrix(eUnitVector)
                 const chargeName = charge.name.replace('Charge', '')
-                const lawLatex = `$$\\vec{E}_{${chargeName}T} = \\frac{kQ}{(r_{${chargeName}T})^2}\\widehat{r}_{${chargeName}T} = \\frac{(9.0 \\times 10^{-9}) \\times (${
+                const lawLatex = `$$\\vec{E}_{${chargeName}T} = \\frac{kQ}{(r_{${chargeName}T})^2}\\widehat{r}_{${chargeName}T} = \\frac{(9.0 \\times 10^{9}) \\times (${
                     charge.q
-                } \\times 1.602^{-19})}{(${getVectorDistance(
+                } \\times e)}{(${getVectorDistance(
                     charge.position,
                     testCharge.position
-                ).toFixed(2)})^2}${rUnitVectorLatexMatrix} = ${getVectorSize(
-                    e
-                ).toFixed(2)}${eUnitVectorLatexMatrix} \\frac{N}{C}$$`
+                ).toFixed(
+                    2
+                )})^2}${rUnitVectorLatexMatrix}$$\n $$\\hspace{2.55cm}= ${buildLatexScientificNotation(
+                    getVectorSize(e),
+                    0,
+                    ''
+                )}${eUnitVectorLatexMatrix}\\ \\frac{N}{C}$$`
                 return (
                     <Equation key={`electric-field-from-${charge.name}`}>
                         <Latex>{lawLatex}</Latex>
@@ -87,10 +129,46 @@ const CalculationResultSection: React.FC<CalculationResultSectionProps> = ({
 }
 
 const buildLatexUnitVectorMatrix = (vector: Vector) => {
+    if ((vector.x == 0 || !vector.x) && (vector.y == 0 || !vector.y)) {
+        return ''
+    }
     return `\\begin{bmatrix}
   ${vector.x.toFixed(2)} \\\\
   ${vector.y.toFixed(2)}
 \\end{bmatrix}`
+}
+
+const buildLatexScientificNotation: (
+    value: number,
+    accumulator: number,
+    constantMultipler: string
+) => string = (
+    value: number,
+    accumulator: number,
+    constantMultipler: string
+) => {
+    if (value == 0 || isNaN(value)) {
+        return '0'
+    }
+    if (!isFinite(value)) {
+        return 'value\\ is\\ too\\ big'
+    }
+    if (value >= 10) {
+        return buildLatexScientificNotation(
+            value / 10,
+            accumulator + 1,
+            constantMultipler
+        )
+    } else if (value < 1) {
+        return buildLatexScientificNotation(
+            value * 10,
+            accumulator - 1,
+            constantMultipler
+        )
+    }
+    return `${value.toFixed(
+        2
+    )}${constantMultipler} \\times ${10}^{${accumulator}}`
 }
 
 const Wrapper = styled.div`
