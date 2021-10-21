6732e1ae593ceab4d7ec8fba468804ce06323b22
Wicharn Rueangkhajorn <wicharn.keisei@gmail.com>
2021-10-21 09:24:56 +0700

    feat: sort the charges before set to state

--- hooks/usePointCharges.ts
+++ hooks/usePointCharges.ts
@@ -1,36 +1,59 @@
-import { PointCharge } from "cs-zeus";
-import { useState } from "react";
+import { PointCharge } from 'cs-zeus';
+import { useState } from 'react';
 
 export const usePointCharges = () => {
-  const initialCharges = [
-    { name: 'Test Charge', position: { x: 0, y: 0 }, q: 1 },
-    { name: 'Charge 1', position: { x: 1, y: 1 }, q: -1 },
-    { name: 'Charge 2', position: { x: 1, y: 2 }, q: 1 },
-  ];
+	const initialCharges = [
+		{ name: 'Test Charge', position: { x: 0, y: 0 }, q: 1 },
+		{ name: 'Charge 1', position: { x: 1, y: 1 }, q: -1 },
+		{ name: 'Charge 2', position: { x: 1, y: 2 }, q: 1 },
+	];
 
-  const [pointCharges, setPointCharges] = useState<PointCharge[]>(initialCharges);
+	const [pointCharges, setPointCharges] =
+		useState<PointCharge[]>(initialCharges);
 
-  const addPointChargeHandler = (q: number) => {
-    setPointCharges((prev) => [...prev, { name: `Charge ${prev.length + 1}`, position: { x: 0, y: 0 }, q }]);
-  };
+	const addPointChargeHandler = (q: number) => {
+		setPointCharges((prev) => [
+			...prev,
+			{ name: `Charge ${prev.length + 1}`, position: { x: 0, y: 0 }, q },
+		]);
+	};
 
-  const editChargeHandler = (charge: PointCharge) => {
-    setPointCharges((prev) => [
-      ...prev.filter((c) => c.name !== charge.name),
-      charge,
-    ]);
-  };
+	const editChargeHandler = (charge: PointCharge) => {
+		setPointCharges((prev) => [
+			...prev.filter((c) => c.name !== charge.name),
+			charge,
+		]);
+	};
 
-  const editPositionHandler = (charge: PointCharge) => {
-    setPointCharges((prev) => [
-      ...prev.filter((c) => c.name !== charge.name),
-      charge,
-    ]);
-  };
+	const editPositionHandler = (charge: PointCharge) => {
+		setPointCharges((prev) =>
+			[...prev.filter((c) => c.name !== charge.name), charge].sort(
+				(charge1, charge2) => {
+					var name1 = charge1.name.toUpperCase();
+					var name2 = charge2.name.toUpperCase();
+					if (name1 < name2) {
+						return -1;
+					}
+					if (name1 > name2) {
+						return 1;
+					}
+					return 0;
+				}
+			)
+		);
+	};
 
-  const removePointChargeHandler = (chargeName: string) => {
-    setPointCharges((prev) => prev.filter((charge) => charge.name !== chargeName));
-  };
+	const removePointChargeHandler = (chargeName: string) => {
+		setPointCharges((prev) =>
+			prev.filter((charge) => charge.name !== chargeName)
+		);
+	};
 
-  return { pointCharges, addPointChargeHandler, editChargeHandler, editPositionHandler, removePointChargeHandler };
-}
+	return {
+		pointCharges,
+		addPointChargeHandler,
+		editChargeHandler,
+		editPositionHandler,
+		removePointChargeHandler,
+	};
+};
