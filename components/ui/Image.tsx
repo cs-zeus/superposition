746fc2e68b6db905ffbd211ddeb00ea5a080fce0
zielu92 <zielu0@gmail.com>
2021-10-24 10:29:24 +0700

    feat(ui): add image component

---
+++ components/ui/Image.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+const Image =  styled.img`
+  border: 2px solid black;
+  margin-left: auto;
+  margin-right: auto;
+  display: block;
+`;
+
+export default Image;
