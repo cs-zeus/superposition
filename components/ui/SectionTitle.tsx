7879ee8de0c1af2f69ebf48117987c0a4ac9833d
Pittawat <pittawat2542@gmail.com>
2021-10-27 19:35:25 +0700

    style: improve spacing

--- components/ui/SectionTitle.tsx
+++ components/ui/SectionTitle.tsx
@@ -1,10 +1,10 @@
 import styled from '@emotion/styled';
 
-
 const SectionTitle = styled.h1`
   font-size: 1.75rem;
   color: var(--white);
   font-weight: bold;
+  margin-bottom: 16px;
 `;
 
 export default SectionTitle;
