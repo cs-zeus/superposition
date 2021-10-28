d4be6d68f50c30fe27a7df90bec799ff7a5b779c
Pittawat2542 <Pittawat2542@users.noreply.github.com>
2021-10-28 13:37:40 +0000

    deploy: d00bb661aad04d401afc9bcc2c46390a403d7f7b

---
+++ _next/static/VfP-Hla1q8QgSo3PdEJMq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/812cee03-cf50e0923c48f72d.js","static/chunks/520-92623add8f9afe3e.js","static/chunks/pages/index-deef20bbab115ed0.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
