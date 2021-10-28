772554c1c6001895ce0b01125ed56a94c1f8b474
Pittawat <pittawat2542@gmail.com>
2021-10-28 20:22:38 +0700

    chore: add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,11 @@ class AppDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=optional"
             rel="stylesheet"
           />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="icon" href="/favicon.ico" sizes="any" />
+          <link rel="manifest" href="/site.webmanifest" />
         </Head>
         <body>
           <Main />
